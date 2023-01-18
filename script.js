668b21ad509fc2af7d626434dd0f5170772f9270
Titillochon <105912183+Titillochon@users.noreply.github.com>
2023-01-18 14:40:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const textArea = document.querySelector("textarea");
+const playButton = document.querySelector("button");
+const pitchBar = document.querySelector("input");
+const duckFigure = document.querySelector("figure");
+
+//get the text from the text area that user entered
+//Make the duck say those workds when the button is clicked
+
+function onButtonClick() {
+  if (textArea.value.length > 0) {
+    speak();
+  }
+}
+
+function speak() {
+  const text = textArea.value;
+
+  const utterance = new SpeechSynthesisUtterance(text);
+  //Must be between 0 and 2
+
+  utterance.pitch = pitchBar.value;
+
+  speechSynthesis.speak(utterance);
+
+  utterance.addEventListener("start", function () {
+    playButton.disabled = true;
+    textArea.disabled = true;
+    pitchBar.disabled = true;
+    duckFigure.classList.add("talking");
+  });
+
+  utterance.addEventListener("end", function () {
+    playButton.disabled = false;
+    textArea.disabled = false;
+    pitchBar.disabled = false;
+    duckFigure.classList.remove("talking");
+  });
+}
+
+playButton.addEventListener("click", onButtonClick);
